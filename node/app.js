30053c8472ea3b452a0fd41c0f7f6f548a0e8758
albarracing23 <albarracing23@gmail.com>
2022-11-05 14:55:50 -0300

    package-look.json

--- node/app.js
+++ node/app.js
@@ -9,6 +9,7 @@ import logisticarouter from './routes/LogisticaRoute.js'
 import combustiblerouter from './routes/CombustibleRoute.js'
 const app = express()
 
+
 app.use(cors())
 app.use(express.json())
 app.use('/transporte', transporteRoutes)
